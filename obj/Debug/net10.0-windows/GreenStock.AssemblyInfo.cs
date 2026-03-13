79f5a562b4395c8019852b1422b6c6cd1a5e62b1
1justauser <superrandommmm@gmail.com>
2026-03-14 00:19:40 +0300

    сделаны задания до T-17, необходимо обновление дизайна

--- obj/Debug/net10.0-windows/GreenStock.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/GreenStock.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreenStock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6cbcd44299f546c58d942e1488623607ea6614")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e929f37191bfc2c5261a2c96ef309de1a2b681e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreenStock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreenStock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
